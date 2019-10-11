060c740f7d38a486385558554890df80f67dbd1a
harshmittal99 <32714380+harshmittal99@users.noreply.github.com>
2019-10-11 14:19:01 +0530

    added new stack code

---
+++ Stacks/stack1.cpp
@@ -0,0 +1,16 @@
+//Deepak Aggarwal, Coding Blocks
+//deepak@codingblocks.com
+
+#include <iostream>
+using namespace std;
+template <class T>
+template <typename T,typename C>
+class Stack{
+	class node{
+	public:
+
+	}
+	node * top;
+	int sze;
+
+}
